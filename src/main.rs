66cf7f844e8451fa76aadcc84190d3c75006faa2
“niladrix719” <niladrix719@gmail.com>
2025-07-03 02:16:11 +0530

    feat: Resp conversion implemented
    Signed-off-by: “niladrix719” <niladrix719@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,56 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
+mod parser;
+
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
-
     loop {
         let (mut stream, _) = listener.accept().await?;
-
         tokio::spawn(async move {
             let mut buf = [0; 512];
-
             loop {
                 match stream.read(&mut buf).await {
                     Ok(0) => break,
-                    Ok(_) => {
-                        if let Err(e) = stream.write_all(b"+PONG\r\n").await {
-                            eprintln!("Write error: {}", e);
-                            break;
+                    Ok(n) => {
+                        let mut parser = parser::Resp::new(&buf[..n]); // only parse filled bytes
+                        let resp = match parser.parse_texts() {
+                            Ok(r) => r,
+                            Err(e) => {
+                                eprintln!("Parse error: {}", e);
+                                continue;
+                            }
+                        };
+                        if resp.is_empty() {
+                            continue;
+                        }
+                        match resp[0].as_str() {
+                            "PING" => {
+                                if let Err(e) = stream.write_all(b"+PONG\r\n").await {
+                                    eprintln!("Write error: {}", e);
+                                    break;
+                                }
+                            }
+                            "ECHO" => {
+                                if resp.len() < 2 {
+                                    eprintln!("ECHO missing argument");
+                                    continue;
+                                }
+                                let msg = &resp[1];
+                                let response = format!("+{}\r\n", msg);
+                                if let Err(e) = stream.write_all(response.as_bytes()).await {
+                                    eprintln!("Write error: {}", e);
+                                    break;
+                                }
+                            }
+                            _ => {
+                                let err_msg = format!("-ERR unknown command '{}'\r\n", resp[0]);
+                                if let Err(e) = stream.write_all(err_msg.as_bytes()).await {
+                                    eprintln!("Write error: {}", e);
+                                    break;
+                                }
+                            }
                         }
                     }
                     Err(e) => {
@@ -29,4 +62,3 @@ async fn main() -> std::io::Result<()> {
         });
     }
 }
-
---
+++ src/parser.rs
@@ -0,0 +1,64 @@
+use std::io::{self, BufReader, Read};
+
+pub struct Resp<R: Read> {
+    reader: BufReader<R>,
+}
+
+impl<R: Read> Resp<R> {
+    pub fn new(rd: R) -> Self {
+        Resp {
+            reader: BufReader::new(rd),
+        }
+    }
+
+    pub fn parse_texts(&mut self) -> Result<Vec<String>, std::io::Error> {
+        let mut buf = [0u8; 1];
+        self.reader.read_exact(&mut buf)?;
+        let sym = buf[0];
+
+        match sym {
+            b'*' => self.parse_array(),
+            _ => Err(io::Error::new(
+                io::ErrorKind::InvalidData,
+                "Invalid response type",
+            )),
+        }
+    }
+
+    pub fn read_line(&mut self) -> Result<String, std::io::Error> {
+        let mut s = String::new();
+        loop {
+            let mut buf = [0u8; 1];
+            self.reader.read_exact(&mut buf)?;
+            let ch = buf[0] as char;
+
+            if ch == '\r' {
+                let mut next = [0u8; 1];
+                self.reader.read_exact(&mut next)?;
+                if next[0] == b'\n' {
+                    break;
+                } else {
+                    s.push(ch);
+                    s.push(next[0] as char);
+                    continue;
+                }
+            } else {
+                s.push(ch);
+            }
+        }
+        Ok(s)
+    }
+
+    pub fn parse_array(&mut self) -> Result<Vec<String>, std::io::Error> {
+        let len_str = self.read_line()?;
+        let len: usize = len_str.parse().unwrap_or(0);
+        let mut elements = Vec::with_capacity(len);
+
+        for _ in 0..len {
+            let nested = self.parse_texts()?;
+            elements.extend(nested);
+        }
+
+        Ok(elements)
+    }
+}
